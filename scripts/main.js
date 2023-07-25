8ec12d2fb20f173037ad35f7f1963368c1978cb7
JoshuaDeKlerk <231207@virtualwindow.co.za>
2023-07-26 00:15:12 +0200

    Finishing Slide 1

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+// When the document loads
+$(document).ready(function(){
+
+    console.log("Hello" );
+
+    // When the hero image loads animate it upwards
+    $( ".hero-image" ).animate({top: '-=100px'})
+    
+});
+
