dde7963d9ed03010e37d2b0f17f2a79fac3d44a5
JoshuaDeKlerk <231207@virtualwindow.co.za>
2023-08-02 13:49:50 +0200

    Html and css for wishlist page

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,34 @@ $(document).ready(function(){
 
     console.log("Hello" );
 
+    // -----------------------------------------------------------------------------
+    // Home Page
+
     // When the hero image loads animate it upwards
     $( ".hero-image" ).animate({top: '-=100px'})
     
+    // -----------------------------------------------------------------------------
+    // Browse Page
+
+    // Hide the description text from the plant card
+    $("#descriptionText").hide();
+
 });
+
+// When the plant card is clicked
+$(".card").click(function(){
+
+    // Toggle the price and description text
+    $("#descriptionText").toggle();
+    $("#priceText").toggle();
+
+    // Resize the plant image to fit additional content
+    $(".card-img-top").toggleClass("small");
+
+})
+
+// When the Remove btn is clicked
+$("#removeButton").click(function(){
+
+    // Remove row from function
+})
