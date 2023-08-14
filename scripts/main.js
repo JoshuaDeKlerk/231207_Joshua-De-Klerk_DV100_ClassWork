1077cecf7d7eafe16c918f4c4e78b8fbd16558db
OW Student <student@openwindow.co.za>
2023-08-14 09:55:16 +0200

    Week 4

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,44 @@
+// -----------------------------------------------------------------------------
+// Plants Array
+// -----------------------------------------------------------------------------
+
+const arrPlants = [
+    {
+        name: "Ficus Tree",
+        price: 350,
+        description: "Graceful and lush, this charming indoor plant boasts glossy, emerald-green leaves that effortlessly brightens any space. Easy to care for and standing at an ideal height, our Ficus Tree is the perfect addition to elevate your interior decor.",
+        image: "plant1.png"
+    },
+    {
+        name: "White Sprite Succulent",
+        price: 200,
+        description: "Delicate and captivating, this rare succulent showcases a mesmerizing silver-white hue that gracefully adorns its petite, fleshy leaves.",
+        image: "plant2.png",
+    },
+    {
+        name: "Snake Plant",
+        price: 400,
+        description: "Boasting tall, sleek, and sword-like leaves, this botanical marvel adds a touch of modern flair to any setting.",
+        image: "plant3.png",
+    },
+    {
+        name: "Parlour Palm",
+        price: 350,
+        description: "With its lush, feather-like fronds and compact size, this indoor beauty makes a striking addition to any interior space.",
+        image: "plant4.png",
+    },
+    {
+        name: "Japanese Maple",
+        price: 1200,
+        description: "Known for its stunning foliage that transforms with the seasons, this ornamental tree captivates with its delicate, lacy leaves in vibrant shades of red, orange, or gold.",
+        image: "plant5.png",
+    },
+];
+
+// -----------------------------------------------------------------------------
 // When the document loads
+// -----------------------------------------------------------------------------
+
 $(document).ready(function(){
 
     console.log("Hello" );
@@ -12,9 +52,9 @@ $(document).ready(function(){
     // -----------------------------------------------------------------------------
     // Browse Page
 
-    // Hide the description text from the plant card
-    $("#descriptionText").hide();
-    
+    // Loads plants function
+    loadplants();
+
     // -----------------------------------------------------------------------------
     // Wishlist Page
 
@@ -25,14 +65,50 @@ $(document).ready(function(){
 
 });
 
+// -----------------------------------------------------------------------------
+// Load all plants
+// -----------------------------------------------------------------------------
+
+function loadplants() {
+
+    console.log(arrPlants);
+
+    for (let i = 0; i < arrPlants.length; i++) {
+        const plant = arrPlants[i];
+
+        console.log(plant);
+        
+        // 1: Select the plants container and add the current array plant to it
+        $("#plantsContainer").append($("#plantCardTemplate").html());
+
+        
+        // 2: Create a variable that contains the most recently added plant card
+        let currentChild = $("#plantsContainer").children().eq(i+1);
+
+        // 3: Set the content for current plant card from the plants array
+        $(currentChild).find("#nameText").text(plant.name);
+        $(currentChild).find("#priceText").text(plant.price);
+        $(currentChild).find("#descriptionText").text(plant.description);
+        $(currentChild).find(".card-img-top").attr('src', '../assets/Plants/' + plant.image);
+
+        // 4: Hide the description text from the current card item
+        $(currentChild).find("#descriptionText").hide();
+
+    }
+
+};
+
+// -----------------------------------------------------------------------------
 // When the plant card is clicked
-$(".card").click(function(){
+// -----------------------------------------------------------------------------
+
+$("#plantsContainer").on('click', '.card' , function() {
 
     // Toggle the price and description text
-    $("#descriptionText").toggle();
-    $("#priceText").toggle();
+    $(this).find("#descriptionText").toggle();
+    $(this).find("#priceText").toggle();
 
     // Resize the plant image to fit additional content
-    $(".card-img-top").toggleClass("small");
+    $(this).find(".card-img-top").toggleClass("small");
 
-})
+});
